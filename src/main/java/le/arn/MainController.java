a5240aa407f87c68f17c281145210779ab12552e
kaunjovi <kaunjovi@gmail.com>
2017-02-19 13:18:32 +0530

    The debug does not work. SOP does. Mystery.

--- src/main/java/le/arn/MainController.java
+++ src/main/java/le/arn/MainController.java
@@ -1,5 +1,7 @@
 package le.arn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableAutoConfiguration
 public class MainController {
 
+	final static Logger logger = LoggerFactory.getLogger(MainController.class);
+
 	@RequestMapping("/")
 	String homepage() {
+
+		// TODO: Why is the debug not showing up. But the SOP is.
+		logger.debug("The homepage is hit.");
+		System.out.println("The homepage is hit.");
 		return "index";
 	}
 
 	public static void main(String[] args) throws Exception {
+		logger.debug("Springboot booting up ...");
 		SpringApplication.run(MainController.class, args);
 	}
 }
